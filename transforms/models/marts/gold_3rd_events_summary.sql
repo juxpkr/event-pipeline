920c28b95e65103d5c955dc4511eb9beef6ad55c
Juseong <juxpkr@gmail.com>
2025-09-19 02:36:12 +0900

    feat: gold_3rd 이벤트 요약 모델 증분 처리로 성능 개선

--- transforms/models/marts/gold_3rd_events_summary.sql
+++ transforms/models/marts/gold_3rd_events_summary.sql
@@ -1,6 +1,39 @@
 -- models/marts/gold_3rd_events_summary.sql
 -- [골드 테이블] 사건 레벨
 
--- WITH events_mapped AS (
---     SELECT * FROM {{ ref('stg_seed_mapping') }}
--- )
+-- 증분 모델 (Incremental Model) 설정
+{{ config(
+    materialized='incremental',
+    unique_key=['global_event_id']
+) }}
+
+WITH events AS (
+    -- 증분 처리를 위해 Silver 레이어 테이블을 직접 참조
+    SELECT * FROM {{ ref('stg_gdelt_microbatch_events') }}
+)
+
+SELECT
+    global_event_id,
+    event_date,
+    actor1_name,
+    actor2_name,
+    event_code,
+    goldstein_scale,
+    avg_tone,
+    num_mentions,
+    num_sources,
+    num_articles,
+    processed_at
+
+FROM
+    events
+WHERE
+    event_date IS NOT NULL
+
+{% if is_incremental() %}
+
+  -- 이 모델이 이미 데이터를 가지고 있다면,
+  -- 최신 날짜보다 더 새로운 데이터만 처리 (3일 버퍼 포함)
+  AND event_date >= date_add('day', -3, (SELECT MAX(event_date) FROM {{ this }}))
+
+{% endif %}
