4ef193016fc280f6d3ad67113cd37b1c9fb9287f
Juseong <juxpkr@gmail.com>
2025-09-19 02:35:42 +0900

    feat: microbatch events staging을 증분 처리로 성능 향상

--- transforms/models/staging/stg_gdelt_microbatch_events.sql
+++ transforms/models/staging/stg_gdelt_microbatch_events.sql
@@ -1,3 +1,9 @@
+-- 증분 모델 (Incremental Model) 설정
+{{ config(
+    materialized='incremental',
+    unique_key=['global_event_id']
+) }}
+
 SELECT
     global_event_id,
     day as event_date,
@@ -9,6 +15,14 @@ SELECT
     1 as event_count
 FROM
     {{ source('gdelt_silver_layer', 'gdelt_events')}}
-WHERE 
+WHERE
     global_event_id IS NOT NULL
     AND day IS NOT NULL
+
+{% if is_incremental() %}
+
+  -- 이 모델이 이미 데이터를 가지고 있다면,
+  -- 최신 날짜보다 더 새로운 데이터만 처리 (3일 버퍼 포함)
+  AND processed_at >= date_add('day', -3, (SELECT MAX(processed_at) FROM {{ this }}))
+
+{% endif %}
