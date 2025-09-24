b5eb621d5f19857dc206bac6b2a659d512556a25
JeongJY <kkabamsky@gmail.com>
2025-09-24 22:15:11 +0900

    feat: Edit staging and marts dbt models

--- transforms/models/staging/stg_gkg_detailed_events.sql
+++ transforms/models/staging/stg_gkg_detailed_events.sql
@@ -1,11 +1,9 @@
--- [Staging 테이블]: models/staging/stg_gkg_detailed_events.sql
+-- [Staging Layer] : models/staging/stg_gkg_detailed_events.sql
+-- Version : 2.0
 -- GKG 및 Mentions 상세 정보 정제
+-- View로 작동하므로, 증분 관련 코드를 제거함
 
--- 증분 모델 (Incremental Model) 설정
-{{ config(
-    materialized='incremental',
-    unique_key=['global_event_id']
-) }}
+{{ config(materialized='view')}}
 
 WITH source_data AS (
     SELECT * FROM {{ source('gdelt_silver_layer', 'gdelt_events_detailed') }}
@@ -30,9 +28,3 @@ SELECT
 
 FROM
     source_data
-
-{% if is_incremental() %}
-WHERE
-    -- 이 모델이 이미 데이터를 가지고 있다면, 최신 날짜보다 더 새로운 데이터만 처리
-    processed_at > (SELECT MAX(processed_at) FROM {{ this }})
-{% endif %}
