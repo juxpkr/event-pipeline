e25eee09011c43b747b51a4948a96e79eae3d4d2
Juseong <juxpkr@gmail.com>
2025-09-27 02:24:40 +0900

    fix: kafka 세팅 수정

--- src/ingestion/gdelt_bronze_consumer.py
+++ src/ingestion/gdelt_bronze_consumer.py
@@ -109,7 +109,7 @@ def setup_streaming_query(spark: SparkSession, data_type: str, logger):
         .option("kafka.bootstrap.servers", kafka_bootstrap_servers)
         .option("subscribe", kafka_topic)
         .option("startingOffsets", "earliest")
-        .option("failOnDataLoss", "true")
+        .option("failOnDataLoss", "false")
         .load()
     )
 
